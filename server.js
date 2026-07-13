0b3c152ce441368a93e1fe7b3c7d44342b6eecb4
mucaii <131178627+mucaii@users.noreply.github.com>
2026-07-13 21:17:29 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+    console.log("Nekdo se je povezal!");
+
+    socket.on("message", (msg) => {
+        console.log(msg);
+
+        io.emit("message", msg);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Uporabnik je odšel.");
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Server teče na http://localhost:3000");
+});
+
